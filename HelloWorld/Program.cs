43c68cff24e7beaa89a8a6b282362234085e9b1a
“Yilber <yilberandrey@gmail.com>
2017-03-22 14:45:31 -0500

    Se agrego a pompilio

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,19 +11,22 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world"); //atajo cw
-            
-
             Persona extraño1 = new Persona("Yilber",21,"2557001");
             string datos = extraño1.DatosPersona();
             Console.WriteLine(datos);
-
-            Console.WriteLine("Chao mundo");
+            Console.WriteLine("Bye world");
 
             Console.WriteLine("Hola mundo");
-
             Persona extraño2 = new Persona("Sebastian", 21, "2557001");
             datos = extraño1.DatosPersona();
             Console.WriteLine(datos);
+            Console.WriteLine("Chao mundo");
+
+            Console.WriteLine("konichiwa");
+            Persona extraño3 = new Persona("Pompilio", 21, "2557001");
+            datos = extraño1.DatosPersona();
+            Console.WriteLine(datos);
+            Console.WriteLine("Sayonara");
 
             Console.ReadKey();
         }
